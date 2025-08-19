653064e21ab349edefda9c57a6e1bd7688977a8e
AnaBheatriz22 <bheatrisa979@gmail.com>
2025-08-18 22:25:42 -0300

    finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import MyName from './components/MyName'
-import Pessoa from './components/Pessoa'
-import styles from './components/Pessoa.module.css';
+import Pessoa from './components/Pessoa';
+
 
 function App() {
 
@@ -9,16 +9,21 @@ function App() {
 
   return (
     <div>
-      <h1>Atividade Perfil Pessoal</h1>
+      <h1>Meu</h1>
       <Pessoa
+      
         nome="Ana Bheatriz Da Silva Santos"
         idade="18"
-        formacaoacademica="Ensino Médio Completo -
-        Curso de informática - Infocurso (Word, Excel, PowerPoint, Photoshop)"
+        estadocivil="Solteira"
+        formacaoacademica="Ensino Médio Completo - Curso de informática - Infocurso (Word, Excel, PowerPoint, Photoshop)"
         foto= "public/Captura de tela 2025-07-14 230838.png"
-        quemsou= "Atualmente, sou estudante de Análise e Desenvolvimento de sistemas na ETEC de Itanháem, Desde os meu 13 anos vim desenvolvendo afinidade com a tecnologia e gosto de explorar diferentes ferramentas digitais, telho dominio intermediário do pacote office e estudando mais em logicas de programação, a linguagem que vim tendo mais afinidade do curso de ADS é a PHP. Tenho bastante afinidade também é o uso do Photoshop."
+        telefone="(13)98864-5886"
+        email="anabheatrizdasilvasantos@gmail.com"
+        genero="Feminino"
+        dtnasci="22/02/2007"
+        quemsou= " Atualmente, sou estudante de Análise e Desenvolvimento de sistemas na ETEC de Itanháem, Desde os meu 13 anos vim desenvolvendo afinidade com a tecnologia e gosto de explorar diferentes ferramentas digitais, telho dominio intermediário do pacote office e estudando mais em logicas de programação, a linguagem que vim tendo mais afinidade do curso de ADS é a PHP. Tenho bastante afinidade também é o uso do Photoshop"
+        cpf="553.345.008-47"
       />
-     
     </div>
   )
 }
--- src/components/Pessoa.jsx
+++ src/components/Pessoa.jsx
@@ -1,17 +1,21 @@
 
-function Pessoa({nome, idade, profissao, foto, formacaoacademica, email, quemsou}) {
+function Pessoa({nome, idade, foto, formacaoacademica, estadocivil, email, quemsou, genero, telefone, dtnasci, cpf }) {
 
         return(
             <div>
                 <h2> Perfil Pessoal</h2>
-                <img src={foto}  width="600" height="700"/>
+                <img src={foto}  width="600" height="700" />
+          
                 <h3> Nome: {nome}</h3>
                 <p> Idade: {idade}</p>
-                <p>Email:</p>
-                <p>Quem sou:</p>
+                <p>Data De Nascimento:{dtnasci}</p>
+                <p>Gênero:{genero}</p>
+                <p>Telefone:{telefone}</p>
+                <p>Estado Civil:{estadocivil}</p>
+                <p>Email:{email}</p>
+                <p>Quem sou:{quemsou}</p>
                 <p>Formação Acadêmica: {formacaoacademica}</p>
-
-
+                <p>CPF:{cpf}</p>
             </div>
         )
 }
